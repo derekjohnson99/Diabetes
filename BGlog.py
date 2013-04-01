50fde6b2e7093a90d553c18648ba8f73b5512d8e
derekjohnson99 <derekjohnson99@gmail.com>
2013-04-01 20:19:32 +0100

    Extract log data and create a list of BG readings.

--- BGlog.py
+++ BGlog.py
@@ -7,6 +7,7 @@
 # Description: Read Exported Glucose Buddy log and print table of BG values.
 import sys
 import csv
+from pprint import pprint
 
 logfile = open("/Users/derekjohnson/Dropbox/Diabetes/MyExportedGlucoseBuddyLogs.csv")
 
@@ -24,12 +25,19 @@ class BGreading():
         self.date = extract_date(date_time)
         self.time = extract_time(date_time)
         self.event = event
+    def toString(self):
+        return "%s: %s %2.1f [%s]" % (self.date, self.time, self.value, self.event)
 
 if __name__ == "__main__":
+    BGreadings = []
     try:
         reader = csv.DictReader(logfile)
         for row in reader:
             if row['Type'] == 'BG':
-                print "%s %0.1f %s" % (row['Date Time'], float(row['Value']), row['Event'])
+                BGreadings.append(BGreading(row['Value'], row['Date Time'], row['Event']))
+                #print "%s %0.1f %s" % (row['Date Time'], float(row['Value']), row['Event'])
     finally:
         logfile.close()
+
+for reading in BGreadings:
+    print reading.toString()
