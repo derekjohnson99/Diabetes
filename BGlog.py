0595732cf8dd92b6b86f1075296f997a55cb4776
derekjohnson99 <derekjohnson99@gmail.com>
2013-04-02 19:05:20 +0100

    Create an output CSV file
    Each row represents a day.
    There is a column for each event type.

--- BGlog.py
+++ BGlog.py
@@ -2,7 +2,7 @@
 
 # BGlog.py
 # Derek Johnson
-# 01 Apr 2013 
+# 01 Apr 2013
 #
 # Description: Read Exported Glucose Buddy log and print table of BG values.
 import sys
@@ -26,28 +26,60 @@ def extract_time(date_time = '03/14/2007 09:25:00'):
     sec = int(date_time[17:19])
     return datetime.time(hour, minute, sec)
 
-class BGreading():
+class BGreading(object):
     def __init__(self, value, date_time, event):
         self.value = float(value)
         self.date = extract_date(date_time)
         self.time = extract_time(date_time)
         self.event = event
     def toString(self):
-        return "%s: %2.1f [%s]" % (self.time, self.value, self.event)
+        return "%s: %2.1f" % (self.event, self.value)
+    def getValue(self):
+        return self.value
     def getDate(self):
-        return self.date.__str__()
+        return self.date
+    def getEvent(self):
+        return self.event
+    def getTime(self):
+        return self.time
+    def beforeNoon(self):
+        noon = datetime.time(12, 0, 0)
+        return (self.time < noon)
 
 if __name__ == "__main__":
+    event_order = ['Out Of Bed',
+                   'During Night',
+                   'Before Breakfast',
+                   'After Breakfast',
+                   'Before Lunch',
+                   'After Lunch',
+                   'Before Dinner',
+                   'After Dinner',
+                   'Before Bed']
     BGreadings = {}
     try:
         reader = csv.DictReader(logfile)
         for row in reader:
             if row['Type'] == 'BG':
                 reading = BGreading(row['Value'], row['Date Time'], row['Event'])
-                date = reading.getDate()
+                date = str(reading.getDate())
                 BGreadings[date] = BGreadings.get(date, [])
-                BGreadings[date].append(reading.toString())
+                BGreadings[date].append(reading)
     finally:
         logfile.close()
 
-    pprint(BGreadings)
+    #pprint(BGreadings)
+    print "Date,%s" % ','.join(event_order)
+    dates = BGreadings.keys()
+    dates.sort()
+    for date in dates:
+        events = {}
+        for reading in BGreadings[date]:
+            e = str(reading.getEvent())
+            events[e] = events.get(e, [])
+            events[e].append(str(reading.getValue()))
+        
+        line = []
+        for e in event_order:
+            line.append("|".join(events.get(e, '')))
+        print "%s,%s" % (date, ','.join(line))
